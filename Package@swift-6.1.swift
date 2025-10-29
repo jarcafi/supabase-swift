1a9cc1f2e81fcc197f0cdbdace5be48d793bcd27
jaro <jaro.fitzi@hotmail.com>
2025-10-29 17:42:14 +0100

    Update Swift 6.1 manifest to use jarcafi forks

--- Package@swift-6.1.swift
+++ Package@swift-6.1.swift
@@ -34,11 +34,11 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"5.0.0"),
     .package(url: "https://github.com/apple/swift-http-types.git", from: "1.3.0"),
-    .package(url: "https://github.com/pointfreeco/swift-clocks", from: "1.0.0"),
-    .package(url: "https://github.com/pointfreeco/swift-concurrency-extras", from: "1.1.0"),
-    .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.3.2"),
-    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.17.0"),
-    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.2.2"),
+    .package(url: "https://github.com/jarcafi/swift-clocks.git", branch: "main"),
+    .package(url: "https://github.com/jarcafi/swift-concurrency-extras.git", branch: "main"),
+    .package(url: "https://github.com/jarcafi/swift-custom-dump.git", branch: "main"),
+    .package(url: "https://github.com/jarcafi/swift-snapshot-testing.git", branch: "main"),
+    .package(url: "https://github.com/jarcafi/xctest-dynamic-overlay.git", branch: "main"),
     .package(url: "https://github.com/WeTransfer/Mocker", from: "3.0.0"),
   ],
   targets: [
